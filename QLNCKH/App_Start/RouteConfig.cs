e7d0ae6cc2b4124712622ec03062443fbb3e584a
ducro76 <ductrang2005@gmail.com>
2026-04-10 17:51:29 +0700

    xoa het trang chu roi de muc chinh la login

--- QLNCKH/App_Start/RouteConfig.cs
+++ QLNCKH/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace QLNCKH
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "TrangChu", action = "TrangChu", id = UrlParameter.Optional }
+                defaults: new { controller = "User", action = "Login", id = UrlParameter.Optional }
                 ,namespaces: new[] { "QLNCKH.Controllers" }
             );
         }
